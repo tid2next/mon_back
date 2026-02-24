acec29f2e16744cc0a9fa971bf9f18fa9f6f067f
gbanet2009-web <gbanet2009@gmail.com>
2026-02-24 02:34:19 +0000

    Update settings.py

--- school_doc_backend/settings.py
+++ school_doc_backend/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------------------------------------------
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-key-for-school-doc-proj')
 DEBUG = False  # ⚠️ Toujours False en prod !
-ALLOWED_HOSTS = [*]
-# ------------------------------------------------------------
+ALLOWED_HOSTS = ['*'] ----------------------------------------------------------
 # INSTALLED APPS
 # ------------------------------------------------------------
 INSTALLED_APPS = [
