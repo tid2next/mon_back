645ac22eba33aa4e24437462f6bbd2570b741b1e
gbane2009-web <gbanet2009@gmail.com>
2026-02-24 00:31:15 +0000

    hhrf

--- school_doc_backend/settings.py
+++ school_doc_backend/settings.py
@@ -78,7 +78,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
